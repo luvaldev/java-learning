7652793c81e32548b55eafe93357f4d8a070133f
luvaldev <luisvmondaca@gmail.com>
2024-03-04 08:24:13 -0300

    Edit

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+//Clase publica 
+public class Main {
+  //Clase publica, estatica de tipo void que no retorna llamada "main" con argumentos de tipo string
+  public static void main(String[] arg) {
+    System.out.println("Hola mundo");
+    //Metodologia para imprimir linea de texto en java
+  }
+}
