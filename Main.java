dc5adf2e62e998d2d4776900bc24e52f35335089
luvaldev <luisvmondaca@gmail.com>
2024-03-13 23:33:27 -0300

    Modf

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 
 //Clase publica 
 public class Main {
+
+  /* --------------- Imprimir Texto --------------- */
+
   //Clase publica, estatica de tipo void que no retorna llamada "main" con argumentos de tipo string
   public static void main(String[] arg) {
     System.out.println("Hola mundo");
     //Metodologia para imprimir linea de texto en java
   }
+
+
+  /* --------------- TIPOS DE DATOS --------------- */
+
+
+
 }
+
